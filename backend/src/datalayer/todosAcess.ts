a796a5301c05279d3b015b05d8da8ef2e93549fe
tOgunfile <tosinogunfile@gmail.com>
2022-10-07 05:36:41 +0100

    Removed lingering comments

--- backend/src/datalayer/todosAcess.ts
+++ backend/src/datalayer/todosAcess.ts
@@ -1,9 +1,5 @@
 import "source-map-support/register";
-
-// import * as AWS from 'aws-sdk'
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
-// import * as AWSXRay from 'aws-xray-sdk'
-
 import { TodoItem } from "../models/TodoItem";
 import { TodoUpdate } from "../models/TodoUpdate";
 import { createLogger } from "../utils/logger";
