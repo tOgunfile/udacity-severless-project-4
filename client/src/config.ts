be573249a1cddd9845f8073601c33f15e144e381
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-10-07 16:08:16 +0100

    Corrected empty todo creation

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'gg1dy1vw4l'
+const apiId = 'ya0akrx9c8'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
