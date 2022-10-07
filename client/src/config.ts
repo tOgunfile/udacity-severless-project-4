e080a35dce9112c292582af1d71169373c909b0f
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-10-07 14:05:14 +0100

    Updated apiID in client

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '1ixi0471y7'
+const apiId = 'gg1dy1vw4l'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
